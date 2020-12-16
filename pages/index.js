ed54dc8b2d5b02d8111f482776f96a377813bed5
Lisha Tawar <75314332+lishatawar@users.noreply.github.com>
2020-12-16 16:58:47 +0530

    shopify app node added

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+// function App() {
+//   return (
+//     <Button variant="contained" color="primary">
+//       Hello World
+//     </Button>
+//   );
+// }
+const Index = () => (
+    <div>
+    <Button variant="contained" color="primary">
+      Welcome to Shopify Application 
+    </Button>
+    </div>
+  );
+export default Index;
