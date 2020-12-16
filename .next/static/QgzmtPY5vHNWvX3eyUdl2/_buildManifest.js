08ca5a650359c57f7048d5b656dc325bbeef65c3
Lisha Tawar <75314332+lishatawar@users.noreply.github.com>
2020-12-16 16:32:47 +0530

    shopify node app added

---
+++ .next/static/QgzmtPY5vHNWvX3eyUdl2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-5cca0fd1a804fb79fffb.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-a68435a6a7ee66b8e5d4.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
